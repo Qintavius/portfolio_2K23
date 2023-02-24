2fea28d82125c4f5fdcb1bf05ebc6b9dba876cae
Qintavius <qtzaba@gmail.com>
2023-02-24 16:42:09 +0100

    [ADD] Create Langage component for langage selection

---
+++ src/components/Langage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Langage.scss";
+
+const Langage = () => {
+  return (
+    <div className="flag">
+      <a href="https://en.quentinquintavius.com/"></a>
+    </div>
+  );
+};
+
+export default Langage;
